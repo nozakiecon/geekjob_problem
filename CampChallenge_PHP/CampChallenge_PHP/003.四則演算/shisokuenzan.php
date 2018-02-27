c8523d077401bba6d83f236bfe881ee9623d885b
DESKTOP-2VLK4RF\sho nozaki <nozakiecon@gmail.com>
2018-02-27 15:53:47 +0900

    PHP単元四則演算の課題提出します

---
+++ CampChallenge_PHP/CampChallenge_PHP/003.四則演算/shisokuenzan.php
@@ -0,0 +1,20 @@
+<?php
+$num1 = 7;
+const T = 30;
+
+// 足し算
+$tasu = T + $num1;
+
+// 引き算
+$hiku = T - $num1;
+
+// 掛け算
+$kake = T * $num1;
+
+// 割り算
+$wari = T / $num1;
+
+echo "$tasu";
+echo "$hiku";
+echo "$kake";
+echo "$wari";
