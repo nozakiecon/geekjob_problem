a08a791955ca284b286caa04430f73e3b6e4a377
DESKTOP-2VLK4RF\sho nozaki <nozakiecon@gmail.com>
2018-02-27 17:56:09 +0900

    単元配列の配列の作成の課題提出します

---
+++ CampChallenge_PHP/CampChallenge_PHP/006.配列/array.php
@@ -0,0 +1,17 @@
+<?php
+$data = array('10', '100', 'soeda', 'hayashi', '-20', '118', 'END');
+
+// １番目「10」を表示
+echo $data[0];
+// 2番目「100」を表示
+echo $data[1];
+// 3番目「soeda」を表示
+echo $data[2];
+// 4番目「hayashi」を表示
+echo $data[3];
+// 5番目「20」を表示
+echo $data[4];
+// 6番目「118」を表示
+echo $data[5];
+// 7番目「END」を表示
+echo $data[6];
