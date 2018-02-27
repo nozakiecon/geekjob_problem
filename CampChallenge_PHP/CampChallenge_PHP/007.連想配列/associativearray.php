d0f7e8f3ed5e0728063415d620c6ba4dae45e702
DESKTOP-2VLK4RF\sho nozaki <nozakiecon@gmail.com>
2018-02-27 18:33:02 +0900

    PHP連想配列単元課題提出します

---
+++ CampChallenge_PHP/CampChallenge_PHP/007.連想配列/associativearray.php
@@ -0,0 +1,14 @@
+<?php
+// PHP連想配列単元課題
+// 以下の順で、連想配列を作成してください。
+// "1"に"AAA", "hello"に"world", "soeda"に"33", "20"に"20"
+$data = array('1'=>'AAA', 'hello'=>'world','soeda'=>'33','20'=>'20');
+
+// 1の値を表示
+echo $data['1'];
+// helloの値を表示
+echo $data['hello'];
+// soedaの値を表示
+echo $data['soeda'];
+// 20の値を表示
+echo $data['20'];
