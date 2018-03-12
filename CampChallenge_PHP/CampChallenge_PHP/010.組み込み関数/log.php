c39cd75827df0c1baf39fe9e3b76c5dec4d0a448
DESKTOP-2VLK4RF\sho nozaki <nozakiecon@gmail.com>
2018-03-12 11:48:34 +0900

    単元組み込み関数の組み込み関数まとめの課題提出します

---
+++ CampChallenge_PHP/CampChallenge_PHP/010.組み込み関数/log.php
@@ -0,0 +1,37 @@
+<?php
+// 講義では紹介されていないPHPの組み込み関数はたくさん存在します。
+// PHPの公式サイトから関数を選び、実際にロジックを作成してみてください。
+// また、この処理を作成するに当たり、下記を必ず実装してください。
+// 1. 処理の経過を書き込むログファイルを作成する。
+// 2. 処理の開始、終了のタイミングで、ログファイルに開始・終了の書き込みを行う。
+// 3. 書き込む内容は、「日時　状況（開始・終了）」の形式で書き込む。
+// 4. 最後に、ログファイルを読み込み、その内容を表示してください。
+// 今回はHTMLタグを無効にするhtmlspecialcharsを使用
+date_default_timezone_set('Asia/Tokyo');
+$now = date('Y/m/d H:i:s');
+// 当日が表示されます
+$start= "$now.htmlspecialcharsの処理を開始";
+
+
+$fp = fopen('log.txt', 'a');
+if ($fp != false) {
+    // ファイルに追記
+    fwrite($fp,$start);
+    // 書いたら、閉じる
+    fclose($fp);
+}
+for($i = 0; $i < 100000; ++$i){
+$weblink ='<a href="https://camp.geekjob.jp/</a>';
+$result = htmlspecialchars($weblink,ENT_QUOTES, 'UTF-8');
+echo $result;
+}
+$now = date('Y/m/d H:i:s');
+$end= "$now.htmlspecialcharsの処理を終了";
+
+$fp = fopen('log.txt', 'a');
+if ($fp != false) {
+    // ファイルに追記
+    fwrite($fp,$end);
+    // 書いたら、閉じる
+    fclose($fp);
+}
