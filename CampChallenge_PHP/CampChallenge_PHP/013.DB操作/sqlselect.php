5849e0a776abe5962d611c8b1bc6eba6938a8424
DESKTOP-2VLK4RF\sho nozaki <nozakiecon@gmail.com>
2018-03-16 16:40:01 +0900

    ＰＨＰ単元ＤＢ操作全情報の取得と表示の課題提出します

---
+++ CampChallenge_PHP/CampChallenge_PHP/013.DB操作/sqlselect.php
@@ -0,0 +1,18 @@
+<?php
+//以下の課題を、PDOを利用して実現してください。
+//課題「テーブルの作成とinsert」で作成したテーブルからSELECT*により全件取得し、画面に取得した全情報を表示してください。
+try {
+$dns =  'mysql:host=localhost;';
+$dns .= 'dbname=Challenge_db;';
+$dns .= 'charset=utf8';
+
+$pdo_obj = new PDO($dns, 'nozakiecon2', 'estest');
+$sql = "SELECT * FROM profiles";
+$pdo_st = $pdo_obj->prepare($sql);
+$pdo_st->execute();
+$datas = $pdo_st->fetchAll(PDO::FETCH_ASSOC);
+var_dump($datas);
+} catch (PDOException $pdo_ex) {
+
+    echo 'DB接続に失敗しました。' . $pdo_ex->getMessage();
+}
